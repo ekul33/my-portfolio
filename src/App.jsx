b76ca3e9071bd0538d104f903e6ca68bd125259f
Luke <111943875+ekul33@users.noreply.github.com>
2025-02-27 02:48:51 +0000

    Added Personal Info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Luke Spanner",
+  title: "Software Engineer",
+  email: "",
+  gitHub: "github.com/ekul33",
+  instagram: "",
+  linkedIn: "linkedin.com/in/luke-spanner",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,20 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computing & IT with Psychology student studying at The Open University. I enjoy Exploring OpenAI tools and intergration with Business tools.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
+  "JavaScript",
+  "React",
+  "Rasberry Pi",
+  "HTML & CSS",
+  "PowerShell",
+  "SQL",
 ];
 
 /**
